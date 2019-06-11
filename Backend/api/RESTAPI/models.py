3c4fd8f3543d8ddeaa78e73d8e71f941474e36d8
Rishab Nahata <risnahat@publicisgroupe.net>
2019-06-11 16:34:51 +0530

    MongoEngine Dependency Added and Models Updated

--- Backend/api/RESTAPI/models.py
+++ Backend/api/RESTAPI/models.py
@@ -1,9 +1,10 @@
-from django.db import models
+from mongoengine import Document, EmbeddedDocument, fields
 
-class DataEntry(models.Model):
-    type = models.CharField(max_length=50)
-    dataEntry = models.TextField(max_length=1000000)
-    arrTime = models.DateTimeField(auto_now_add=True)
+class PerformaceData(Document):
+    category = fields.StringField(required=True)
+    value = fields.TextField(max_length=1000000, required=True)
+    createdAt = fields.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return self.type + str(self.arrTime)
+        return self.type + str(self.createdAt)
+        
