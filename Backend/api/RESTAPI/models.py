31c9719a8c3e8bf8db1de90c49678688a52f7365
Brijesh Dhakar <bridhaka@publicisgroupe.net>
2019-06-11 12:36:55 +0530

    model added

--- Backend/api/RESTAPI/models.py
+++ Backend/api/RESTAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DataEntry(models.Model):
+    type = models.CharField(max_length=50)
+    dataEntry = models.TextField(max_length=1000000)
+    arrTime = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.type + str(self.arrTime)
