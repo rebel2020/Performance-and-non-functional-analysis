9c30d70b42dd3e40015a19dfb002220309adf379
Raja Lochan <rajlocha@publicisgroupe.net>
2019-06-25 15:01:47 +0530

    Set Quaries for LightHouse Data

--- Frontend/src/features/lighthouse/index.js
+++ Frontend/src/features/lighthouse/index.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Sidebar from './Sidebar';
 import Home from './Home';
 import MetricComponent from './Metrics';
-// import { Query } from 'react-apollo';
-// import { TEST } from '../../components/graphql/Queries';
 import './main.scss';
-
-const Lighthouse = props => {
+const Lighthouse = props => {  
   const [toggle, setToggle] = useState(false);
+
   // const [component, changeComponent] = useState('home');
   const { comp } = props;
   const map = {
@@ -25,14 +23,6 @@ const Lighthouse = props => {
         // changeComponent={changeComponent}
       />
       <div className={toggle ? 'main' : 'main-extend'}>{graph}</div>
-      {/* <Query query={TEST}>
-        {({ loading, error, data }) => {
-          if (loading) console.log('loading');
-          if (error) console.log(error.message);
-          if (data) console.log(data);
-          return null;
-        }}
-      </Query> */}
     </>
   );
 };
