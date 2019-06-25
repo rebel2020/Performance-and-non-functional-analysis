12addc822c17ca5cffa8648cdbb04923c56ffcf9
Abishek Padaki <abipadak@publicisgroupe.net>
2019-06-25 09:32:22 +0530

    Made sidebar higher component

---
+++ Frontend/src/features/Sidebar/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Button from 'src/components/button';
+import Link from 'src/components/Link';
+
+import './main.scss';
+
+const Sidebar = props => {
+  let { toggle, setToggle, component, changeComponent } = props;
+  let sideClass = toggle ? 'sidenav' : 'sidenav-collapse';
+  return (
+    <div className={sideClass}>
+      <Button className="sidelink" onClick={() => setToggle(toggle ? false : true)}>
+        {toggle ? 'X' : '='}
+      </Button>
+      <Link className="sidelink" to="/lighthouse">
+        <div className="sidelink">{toggle ? 'Home' : ''}</div>
+      </Link>
+      <Link className="sidelink" to="/lighthouse/performance">
+        <div className="sidelink">{toggle ? 'Performance' : ''}</div>
+      </Link>
+      <Link className="sidelink" to="/lighthouse/accessibility">
+        <div className="sidelink">{toggle ? 'Accessibility' : ''}</div>
+      </Link>
+      {/* <Button className="sidelink" onClick={() => changeComponent('home')}>
+        {toggle ? 'Home' : ''}
+      </Button>
+      <Button className="sidelink" onClick={() => changeComponent('performance')}>
+        {toggle ? 'Performance' : ''}
+      </Button>
+      <Button className="sidelink" onClick={() => changeComponent('accessibility')}>
+        {toggle ? 'Accessibility' : ''}
+      </Button> */}
+    </div>
+  );
+};
+
+export default Sidebar;
