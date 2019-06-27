fdc0a1e4f833c340ee175c6a28fdfadfc61c6376
Sammed Ravanavar <samravan@publicisgroupe.net>
2019-06-27 14:58:20 +0530

    transform graphql response on metrics

--- Frontend/src/features/lighthouse/Home/index.js
+++ Frontend/src/features/lighthouse/Home/index.js
@@ -1,45 +1,51 @@
 import React, { useState, useEffect, useRef } from 'react';
+import useGlobal from 'src/store';
+import previousState from 'src/utilities/previousState';
 import SolidGauge from 'src/components/solidgauge';
+import compare from 'src/utilities/compareObjects';
 import FetchData from 'src/components/graphql/utils';
 import { AVG_LIGHTHOUSE_SCORES } from 'src/components/graphql/Queries';
 import Filters from '../../Filters';
 import './main.scss';
 
-const usePrevious = value => {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-};
-
 const HomeComponent = props => {
+  const [globalState, globalActions] = useGlobal();
+  const { env, brand, page, date } = globalState;
   const [data, setData] = useState({ lighthousedata: [{ audits: {} }] });
   const [query, setQuery] = useState(<></>);
   useEffect(() => {
     setQuery(FetchData(AVG_LIGHTHOUSE_SCORES, setData));
   }, []);
-  // const query = FetchData(AVG_LIGHTHOUSE_SCORES, setData);
-  // const prevState = usePrevious({ url, date });
-  // console.log(prevState);
-  // console.log(data);
-  const { lighthousedata } = data || [{ audits: {} }];
-  const obj = lighthousedata.length ? lighthousedata[0].audits : {};
-  // const values = Object.keys(obj)
-  //   .sort()
-  //   .map(key => obj[key].score);
-  // const values = { performance: 30, accessibility: 40, best_practices: 50, s_e_o: 60, p_w_a: 70 };
-  const values = [0.59, 0.59, 0.59, 0.59, 0.59];
-  console.log(props);
-  const flexItems = ['accessibility', 'best_practices', 'performance', 'p_w_a', 's_e_o'].map(
-    (item, i) => {
-      return (
-        <div key={item}>
-          <SolidGauge name={item} value={Math.round(values[i] * 100)} />
-        </div>
-      );
+  const prevState = previousState({ env, brand, page, date });
+  const onMount = useRef(true);
+  useEffect(() => {
+    if (onMount.current) {
+      onMount.current = false;
+      return;
     }
-  );
+    if (!compare(prevState, { env, brand, page, date })) {
+      console.log(JSON.stringify(prevState), JSON.stringify({ env, brand, page, date }));
+    }
+  });
+
+  console.log(data);
+  const obj = data.lighthousedata[0] ? data.lighthousedata[0].audits : {};
+  const map = {
+    performance: 'performance_audits',
+    best_practices: 'best_practices_audits',
+    p_w_a: 'pwa_audits',
+    s_e_o: 'seo_audits'
+  };
+  const flexItems = ['best_practices', 'performance', 'p_w_a', 's_e_o'].map((item, i) => {
+    return (
+      <div key={item}>
+        <SolidGauge
+          name={item}
+          value={Math.round(obj[map[item]] ? obj[map[item]].score * 100 : '')}
+        />
+      </div>
+    );
+  });
   return (
     <div className="container">
       <Filters date="single" options={['hello', 'react']} />
