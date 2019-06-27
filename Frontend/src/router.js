f69d28f8b57e45944abccfc7a2794f0627708383
Raja Lochan <rajlocha@publicisgroupe.net>
2019-06-27 13:25:46 +0530

    added routedetails

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -1,63 +1,20 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
-import Lighthouse from './features/lighthouse';
-import Gatling from './features/gatling/index';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { ROUTES } from './routeDetails'
 
 const Router = () => {
-  const [toggle, setToggle] = useState(false);
   return (
     <BrowserRouter>
-      <Route
-        exact
-        path="/"
-        render={props => {
-          return <Redirect to={{ pathname: '/lighthouse' }} />;
-        }}
-      />
-      <Route
-        exact
-        path="/gatling"
-        render={({ history }) => {
-          return (
-            <Gatling history={history} comp="gatling_stats" toggle={toggle} setToggle={setToggle} />
-          );
-        }}
-      />
-      <Route
-        exact
-        path="/lighthouse"
-        render={({ history }) => {
-          return <Lighthouse history={history} comp="home" toggle={toggle} setToggle={setToggle} />;
-        }}
-      />
-      <Route
-        exact
-        path="/lighthouse/performance"
-        render={({ history }) => {
-          return (
-            <Lighthouse
-              history={history}
-              comp="performance"
-              toggle={toggle}
-              setToggle={setToggle}
-            />
-          );
-        }}
-      />
-      <Route
-        exact
-        path="/lighthouse/accessibility"
-        render={({ history }) => {
-          return (
-            <Lighthouse
-              history={history}
-              comp="accessibility"
-              toggle={toggle}
-              setToggle={setToggle}
-            />
-          );
-        }}
-      />
+        {   
+            ROUTES.map((route,index) => {
+                return <Route
+                  key = {index}
+                  exact={route.exact}
+                  path={route.path}
+                  render={ ({history}) => route.main(history) } 
+                />
+            })
+        }
     </BrowserRouter>
   );
 };
