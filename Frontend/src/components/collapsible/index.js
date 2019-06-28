fe1d54c417b2d2b30b29134825f3461f5f01ed0b
Abishek Padaki <abipadak@publicisgroupe.net>
2019-06-28 10:11:00 +0530

    Added Material UI styles and changed Audits collapsibles

--- Frontend/src/components/collapsible/index.js
+++ Frontend/src/components/collapsible/index.js
@@ -1,30 +1,116 @@
 import React from 'react';
-import Button from '../button/index';
 
-import './collapse_styles.scss';
-
-class Collapsible extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-    this.togglePanel = this.togglePanel.bind(this);
-  }
-  togglePanel(e) {
-    this.setState({ open: !this.state.open });
-  }
-  render() {
-     const {history} = this.props
-    console.log(history);
-    return (
-      <div className="collapse_container" >
-        <div onClick={e => this.togglePanel(e)} className="header">
-          {' '}
-          Audit_Name <div className="score" >Score: NN</div>
+const Collapsible = props => {
+  const { history, k, title, desc, score, weight, nv, link } = props;
+  console.log(title);
+  return (
+    <>
+      <div className="row">
+        <div className="collapsible col m11">
+          <input type="checkbox" id={`collapsible-${k}`} />
+          <label htmlFor={`collapsible-${k}`}>{title}</label>
+          <div className={`collapsible-${k}-area`}>
+            <div className="row">
+              <p className=" col m6">{desc}</p>
+              <p className="text-right col m6">Score:{score}</p>
+            </div>
+            <br />
+            <div className="row">
+              <p className=" col m6">Weightage:{weight}</p>
+              <p className="text-right col m6">NumericValue:{nv}</p>
+            </div>
+            <br />
+            <div className="row">
+              <button
+                className="btn--raised left col m2"
+                onClick={() => history.push('/lighthouse/' + link)}
+              >
+                Graph
+              </button>
+            </div>
+          </div>
         </div>
-        {this.state.open ? <div className="content"> Content of audit displayed here <br/><br/> <Button className="collapse_link" onClick={()=>history.push('/lighthouse/performance')}>Link_to_graph</Button>  </div> : null}{' '}
       </div>
-    );
-  }
-} 
+    </>
+  );
+};
+// import Button from '../button/index';
+
+// import './collapse_styles.scss';
+
+// class Collapsible extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { open: false };
+//     this.togglePanel = this.togglePanel.bind(this);
+//   }
+//   togglePanel(e) {
+//     this.setState({ open: !this.state.open });
+//   }
+//   render() {
+//     const { history } = this.props;
+//     console.log(history);
+//     return (
+//       <>
+//         <div className="row">
+//           <div className="collapse_container col m6">
+//             <div onClick={e => this.togglePanel(e)} className="header">
+//               {' '}
+//               Audit_Name <div className="score">Score: NN</div>
+//             </div>
+//             {this.state.open ? (
+//               <div className="content">
+//                 {' '}
+//                 Content of audit displayed here <br />
+//                 <br />{' '}
+//                 <Button
+//                   className="collapse_link"
+//                   onClick={() => history.push('/lighthouse/performance')}
+//                 >
+//                   Link_to_graph
+//                 </Button>{' '}
+//               </div>
+//             ) : null}{' '}
+//           </div>
+
+//           <div className="collapse_container col m6">
+//             <div  className="header">
+//               {' '}
+//               Audit_Name <div className="score">Score: NN</div>
+//             </div>
+//             {/* {this.state.open ? (
+//               <div className="content">
+//                 {' '}
+//                 Content of audit displayed here <br />
+//                 <br />{' '}
+//                 <Button
+//                   className="collapse_link"
+//                   onClick={() => history.push('/lighthouse/performance')}
+//                 >
+//                   Link_to_graph
+//                 </Button>{' '}
+//               </div>
+//             ) : null}{' '} */}
+//           </div>
+//         </div>
+//         <div className="row">
+//           <div className="collapse_container col m6">
+//             <div className="header">
+//               {' '}
+//               Audit_Name <div className="score">Score: NN</div>
+//             </div>
+//           </div>
+
+//           <div className="collapse_container col m6">
+//             <div className="header">
+//               {' '}
+//               Audit_Name <div className="score">Score: NN</div>
+//             </div>
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
 export default Collapsible;
