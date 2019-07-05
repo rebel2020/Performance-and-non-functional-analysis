f96a9a0804a4056af97431c0a33b137e43653c35
Abishek Padaki <abipadak@publicisgroupe.net>
2019-07-05 13:51:55 +0530

    Sorted audits based on score

--- Frontend/src/components/collapsible/index.js
+++ Frontend/src/components/collapsible/index.js
@@ -5,33 +5,44 @@ import './styles.scss';
 const Collapsible = props => {
   const { metric, history, k, title, desc, score, weight, nv, link } = props;
   const sch = Math.round(score * 100);
+  let sccol;
+  if (sch <= 25) {
+    sccol = 'low';
+  } else if (sch > 25 && sch <= 80) {
+    sccol = 'mid';
+  } else {
+    sccol = 'high';
+  }
   // console.log(title);
   return (
     <>
       <div className="row">
-        <div className="collapsible collbord col m11">
+        <div className="collapsible collbord col s11">
           <input type="checkbox" id={`collapsible-${k}`} />
-          <label htmlFor={`collapsible-${k}`}>{formatString(title)}</label>
+          <label htmlFor={`collapsible-${k}`}>
+            <div className="col s11">{formatString(title)}</div>
+            <div className={`col s1 right ${sccol}`}>{sch}</div>
+          </label>
+
           <div className={`collbase collapsible-${k}-area`}>
             <div className="row">
               <p className=" col m6">{desc}</p>
-              <p className="text-right col m6">
+              <p className="text-right col s6">
                 Score:&nbsp;
-                {sch}
+                <span className={`${sccol}`}> {sch}</span>
               </p>
             </div>
             <br />
             <div className="row">
-              <p className=" col m6">
+              <p className=" col s6">
                 Weightage:&nbsp;
                 {weight}
               </p>
-              <p className="text-right col m6">
+              <p className="text-right col s6">
                 NumericValue:&nbsp;
                 {nv}
               </p>
             </div>
-            <br />
           </div>
         </div>
       </div>
