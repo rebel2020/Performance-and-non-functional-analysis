dfae9253320f3fcd7ac3723cfbeaf3a6400e6308
Abishek Padaki <abipadak@publicisgroupe.net>
2019-07-12 14:30:52 +0530

    Added Alert functionality for page specific alerts

--- Frontend/src/components/alert_collapsible/index.js
+++ Frontend/src/components/alert_collapsible/index.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import './styles.scss';
+import formatString from '../../utilities/formatString';
 
 const AlertCollapsible = props => {
-  const { k, history, title, desc, category, perc } = props;
+  const { k, history, title, desc } = props;
   let alcol;
-
-  if (perc >= 20) {
-    alcol = 'border-red bg--sidedark color--white';
-  } else {
-    alcol = 'border-yellow bg--sidedark color--white';
+  let scorecol;
+  function compare(a, b) {
+    if (a.scoreDiff > b.scoreDiff) {
+      return -1;
+    }
+    if (a.scoreDiff < b.scoreDiff) {
+      return 1;
+    }
+    return 0;
   }
+  // sorteddesc =
 
+  const description = desc.map((item, i) => {
+    // if (item.scoreDiff > 20) setUrgentData(1);
+    desc.sort(compare);
+    if (item.scoreDiff >= 20) {
+      alcol = ' bg--sidedark color--white';
+      scorecol = 'bg--iored';
+    } else {
+      alcol = 'bg--sidedark color--white';
+      scorecol = 'bg--ioyellow';
+    }
+    return (
+      <React.Fragment key={i}>
+        <div className="row">
+          <p className=" col m6">{formatString(item.name)}</p>
+          <p className=" col m6 ">
+            Average Drop in Score:&nbsp;
+            <div className={`score ${scorecol}`}>{Math.round(item.scoreDiff)}%</div>
+          </p>
+        </div>
+
+        <div className="row">
+          <p className=" col m6">
+            Metric:{` `}
+            {item.category}
+          </p>
+        </div>
+
+        <br />
+      </React.Fragment>
+    );
+  });
   return (
     <>
       <div className="row">
@@ -20,26 +57,14 @@ const AlertCollapsible = props => {
             {title}
           </label>
           <div className={`colar collapsible-${k}-area`}>
-            <div className="row">
-              <p className=" col m6">{desc}</p>
-              <p className=" col m6 ">Average Drop in Score:{perc}%</p>
-            </div>
-            <br />
-            <div className="row">
-              <p className=" col m6">
-                Metric:
-                {category}
-              </p>
-            </div>
-
-            <br />
+            {description}
             <div className="row">
               <button
                 type="button"
                 className="btn--raised btn--red left col m4"
                 onClick={() =>
                   history.push({
-                    pathname: `/lighthouse/Recommendations/PageLevel`
+                    pathname: `/lighthouse/pageLevelRecommendations`
                     // search: `recommendation=${title}`,
                     // recommendation: title
                   })
